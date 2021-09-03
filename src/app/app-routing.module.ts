34d4224f98707228dbecb9bbc94e00b96fc8c3cc
utkarsh2609 <utkarsh.awasthi01@gmail.com>
2021-09-03 17:45:55 +0530

    setting up modules for lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'dashboard', loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule) },
+   { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) }, 
+   { path: 'user', loadChildren: () => import('./user/user.module').then(m => m.UserModule) },
+   { path: 'setting', loadChildren: () => import('./setting/setting.module').then(m => m.SettingModule) }
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
