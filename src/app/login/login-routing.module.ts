b624c2a4c66e6f194abf4c5365967a8b09474882
utkarsh2609 <utkarsh.awasthi01@gmail.com>
2021-09-04 12:10:41 +0530

    working on login module styles

--- src/app/login/login-routing.module.ts
+++ src/app/login/login-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
-const routes: Routes = [{ path: '', component: LoginComponent }];
+const routes: Routes = [
+  { path: '', component: LoginComponent,
+  children:[
+    {path: '', pathMatch: 'full', redirectTo: 'sign-in'},
+    { path: 'sign-in', component: SignInComponent },
+    { path: 'sign-up', component: SignUpComponent },
+  ] 
+},
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -3,11 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 
 @NgModule({
   declarations: [
-    LoginComponent
+    LoginComponent,
+    SignInComponent,
+    SignUpComponent
   ],
   imports: [
     CommonModule,
